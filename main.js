539ec06cc775dfbb731ecbf304c451fcfad86f5c
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-23 23:47:44 -0300

    Objetos

--- main.js
+++ main.js
@@ -1,17 +1,56 @@
-const idade = 27;
-const idade1 = 0;
-const idade2 = null;
+// Objetos
+
+const user = {
+  name: 'Alois',
+  // lastName: 'Hans', -> vai mostrar esse 
+  age: 52,
+  address: {
+    street: 'Rua Miguel Cervantes',
+    number: 458,
+  },
+}
+
+document.body.innerText = ('name' in user)
+document.body.innerText = ('nickname' in user)
+
+document.body.innerText = Object.keys(user)
+document.body.innerText = Object.values(user)
+document.body.innerText = JSON.stringify(Object.values(user))
+document.body.innerText = JSON.stringify(Object.entries(user))
 
 
-// document.body.innerText = 'Sua idade é: ' + idade;
-// document.body.innerText = 'Sua idade é: ' + idade1;
-// document.body.innerText = 'Sua idade é: ' + idade2;
+// Desestruturação
+
+// const address = user.address -> fica assim:
+// const { address } = user
+
+// document.body.innerText = JSON.stringify(address)
+
+// ou com duas:
+// const { address, age } = user
+// document.body.innerText = JSON.stringify({address, age})
+
+// ou renomeando variáveis: 
+// const { address, age: idade } = user
+// document.body.innerText = JSON.stringify({ address, idade})
+
+// const { address, age: idade, lastName = 'Sailer' } = user
+// document.body.innerText = JSON.stringify({ address, idade, lastName})
+
+// function mostrarIdade(user) {
+//   return user.age
+// } -> ou desestruturando 
+
+// function mostrarIdade({ age }) { 
+//   return age
+// }
+
+// document.body.innerText = mostrarIdade(user)
 
 
-// Nullish Coalescing Operator
+// Rest operator
 
-// document.body.innerText = 'Sua idade é: ' +  (idade2 || 'Não informado');
+const { name, age, ...rest } = user
+document.body.innerText = JSON.stringify(rest)
+// não vai retornar name e idade
 
-// 0, '', {}, false, undefined, null => falsy (no caso do operador ou - || )
-
-document.body.innerText = 'Sua idade é: ' +  (idade2 ?? 'Não informado');
