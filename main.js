d2af6cec5601c00154c39a26c23a3287549f95f5
Alois Sailer <alois_sailer@yahoo.com.br>
2022-08-23 23:06:55 -0300

    Nullish Coalescing Operator

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-document.body.innerText = 'hello'
+const idade = 27;
+const idade1 = 0;
+const idade2 = null;
 
+
+// document.body.innerText = 'Sua idade é: ' + idade;
+// document.body.innerText = 'Sua idade é: ' + idade1;
+// document.body.innerText = 'Sua idade é: ' + idade2;
+
+
+// Nullish Coalescing Operator
+
+// document.body.innerText = 'Sua idade é: ' +  (idade2 || 'Não informado');
+
+// 0, '', {}, false, undefined, null => falsy (no caso do operador ou - || )
+
+document.body.innerText = 'Sua idade é: ' +  (idade2 ?? 'Não informado');
